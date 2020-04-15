858ca5a6508883aa4c979185e4c16695a3811459
HazelHex <45799751+HazelHex@users.noreply.github.com>
2020-04-15 15:11:48 +0000

    Added parameter validation

--- clamp.cmd
+++ clamp.cmd
@@ -10,30 +10,31 @@ REM * echo/%percent%
 REM * @param {reference} %1 - Name of variable being clamped and set, value must be number or empty
 REM * @param {number} %2 - Bottom clamp
 REM * @param {number} %3 - Upper clamp
-REM * @param {number} [%4=0] - Default value, if referenced value is empty
-REM * @returns {errorlevel} success (0), check parameters (2)
+REM * @param {number} [%4=0] - Default value to set, if referenced value is empty
+REM * @returns {errorlevel} success (0), invalid parameter (1-4), invalid reference value (5)
 REM */
 
-REM Basic check if required parameters are empty or malformed
-for /l %%p in (1,1,3) do call set /a "%%~%%p" 2>nul || exit /b 2
+REM Parameter validation
+(call if "%%~1"===&&call set "_v=%%~1"&&set _v|findstr/rix "_v=[a-z#$.@[\]_+-][0123456789a-z#$.@[\]_+-]*"||exit/b1)2>nul>nul
+for /l %%v in (2,1,3)do (call if "%%~%%v"===&&call set "_v=%%~%%v"&&set _v|findstr/rix "_v=0 _v=[123456789][0123456789]* _v=-[123456789][0123456789]*"||exit/b%%v)2>nul>nul
+call if "%%~4"===2>nul&&call set "_v=%%~4"&&if defined _v set _v|findstr/rix "_v=0 _v=[123456789][0123456789]* _v=-[123456789][0123456789]*">nul||exit/b4
+if defined %~1 set %~1|findstr/rix "[^=]*=0 [^=]*=[123456789][0123456789]* [^=]*=-[123456789][0123456789]*">nul||exit/b5
+
+REM Get referenced value, if empty set to default
+(call set /a "val = %%%~1%%" || set /a "val = %~4" || set /a "val = 0") 2>nul
 
 REM Swap min and max if needed
 set /a "min = %~2, max = %~3"
 if %min% gtr %max% set /a "min = max, max = %min%"
 
-REM Get referenced value
-call set /a "val = %%%~1%%" 2>nul
-
-if defined val (
-	if %val%0 lss %min%0 ( REM Clamp to min
+if defined %~1 (
+	if %val% lss %min% ( REM Clamp to min
 		set /a "val = min"
-	) else if %val%0 gtr %max%0 ( REM Clamp to max
+	) else if %val% gtr %max% ( REM Clamp to max
 		set /a "val = max"
 	)
-) else ( REM Empty to default
-	set /a "val = %~4" 2>nul || set /a "val = 0"
 )
 
 REM Set referenced variable
-endlocal & set /a "%~1 = %val%"
+endlocal & set "%~1=%val%"
 exit /b 0
