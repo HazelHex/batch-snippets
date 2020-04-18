a75461eb515da9d5bb4d0039dcb747117b947ab1
Vadim <45799751+HazelHex@users.noreply.github.com>
2020-04-18 07:07:06 +0000

    Fixed validation

--- clamp.cmd
+++ clamp.cmd
@@ -15,9 +15,9 @@ REM * @returns {errorlevel} success (0), invalid parameter (1-4), invalid refere
 REM */
 
 REM Parameter validation
-(call if "%%~1"===&&call set "v=%%~1"&&set v|findstr/rix "v=[a-z#$.@[\]_+-][0123456789a-z#$.@[\]_+-]*"||exit/b1)2>nul>nul
-for /l %%v in (2,1,3)do (call if "%%~%%v"===&&call set "v=%%~%%v"&&set v|findstr/rix "v=0 v=[123456789][0123456789]* v=-[123456789][0123456789]*"||exit/b%%v)2>nul>nul
-call if "%%~4"===2>nul&&call set "v=%%~4"&&(if defined v set v|findstr/rix "v=0 v=[123456789][0123456789]* v=-[123456789][0123456789]*">nul)||exit/b4
+(call if "%%~1"===&&call set "0=%%~1"&&set 0|findstr/rix "0=[a-z#$.@[\]_+-][0123456789a-z#$.@[\]_+-]*"||exit/b1)2>nul>nul
+for /l %%v in (2,1,3)do (call if "%%~%%v"===&&call set "0=%%~%%v"&&set 0|findstr/rix "0=0 0=[123456789][0123456789]* 0=-[123456789][0123456789]*"||exit/b%%v)2>nul>nul
+call if "%%~4"===2>nul&&call set "0=%%~4"&&(if defined 0 set 0|findstr/rix "0=0 0=[123456789][0123456789]* 0=-[123456789][0123456789]*">nul)||exit/b4
 if defined %~1 set %~1|findstr/rix "[^=]*=0 [^=]*=[123456789][0123456789]* [^=]*=-[123456789][0123456789]*">nul||exit/b5
 
 REM Get referenced value, if empty set to default
